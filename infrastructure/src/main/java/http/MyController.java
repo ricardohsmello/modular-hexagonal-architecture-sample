f43661d7d0bba0d2f795c5c262f35f5fd8d5d616
Ricardo <ricardohsmello@gmail.com>
2025-05-31 12:32:49 -0300

    first commit

---
+++ infrastructure/src/main/java/http/MyController.java
@@ -0,0 +1,9 @@
+package http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/my-rest")
+public class MyController {
+}
